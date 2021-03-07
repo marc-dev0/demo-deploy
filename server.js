28f647b897354c6401e16a09779d85f6307194bb
marc-dev1 <65924900+marc-dev1@users.noreply.github.com>
2021-03-06 21:18:20 -0500

    update 2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('C:/Users/mroja/Documents/marc/personal/proyectos/demo-deploy/dist/<name-of-app>'));
+app.use(express.static('C:/Users/mroja/Documents/marc/personal/proyectos/demo-deploy/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join('C:/Users/mroja/Documents/marc/personal/proyectos/demo-deploy/dist/<name-of-app>/index.html'));
+res.sendFile(path.join('C:/Users/mroja/Documents/marc/personal/proyectos/demo-deploy/dist/demo-deploy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
